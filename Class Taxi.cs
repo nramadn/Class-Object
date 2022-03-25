695f6c5eb09a4a4582743c3b0a555817f6a39b93
nramadn <nrramadhan12@students.amikom.ac.id>
2022-03-25 13:39:46 +0700

    Praktikum Taxi

---
+++ Class Taxi.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taxi
+{
+    class Taxi
+    {
+        // Properties  
+        public string Nama { get; set; }
+        public string Duty { get; set; }
+        public int Penumpang { get; set; }
+
+        // Method
+        public void TaxiInfo()
+        {
+            Console.WriteLine("Driver Name : {0}", Nama);
+            Console.WriteLine("OnDuty : {0}", Duty);
+            Console.WriteLine("Number of Passengers : {0}", Penumpang);
+        }
+
+        public void PickUpPassenger()
+        {
+            Console.WriteLine("\n{0} Sedang menjemput penumpang", Nama);
+        }
+
+        public void DropOfPassenger()
+        {
+            Console.WriteLine("{0} Selesai mengantar penumpang", Nama);
+        }
+    }
+}
---
+++ Taxi.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taxi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Membuat Object Taxi
+            Taxi taxi = new Taxi();
+
+            // Mengset Nilai Properties objek taxi
+            taxi.Nama = "Royan";
+            taxi.Duty = "Yes";
+            taxi.Penumpang = 13;
+
+            // Memanggil method TaxiInfo, PickUpPassenger, dan DropOfPassenger
+            taxi.TaxiInfo();
+            taxi.PickUpPassenger();
+            taxi.DropOfPassenger();
+
+            Console.ReadKey();
+        }
+    }
+}
